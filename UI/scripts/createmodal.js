8b50c8a310153a9845e31378c32a204624cc021e
Christopher Iliyas <christopheriliyas@gmail.com>
2018-12-19 00:49:10 +0100

    Created admin homepage

---
+++ UI/scripts/createmodal.js
@@ -0,0 +1,15 @@
+'use strict'
+const simpleModal = document.getElementById('simple-modal');
+const modalBtn = document.getElementById('modal-button');
+const closeBtn = document.querySelector('.close_btn');
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+
+function openModal() {
+  simpleModal.style.display = 'block';
+}
+
+function closeModal() {
+  simpleModal.style.display = 'none';
+}
