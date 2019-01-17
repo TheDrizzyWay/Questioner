18c0e0968a963ae97b8dc0245568d97cc645cc43
Christopher Iliyas <christopheriliyas@gmail.com>
2019-01-17 12:42:33 +0100

    docs(api): add inline documentation [finishes #163284901]

--- api/v1/controllers/commentscontroller.js
+++ api/v1/controllers/commentscontroller.js
@@ -3,6 +3,13 @@ import Question from '../models/Questions';
 import { successResponse, errorResponse } from '../utils/responses';
 
 export default {
+  /**
+   * @description Posts a comment to a question
+   * @param  {Object} req - The request object
+   * @param  {object} res - The response object
+   * @returns status code, message and comment details
+   */
+
   createComment: async (req, res) => {
     const newComment = req.body;
 
@@ -18,6 +25,13 @@ export default {
     const result = await newCommentClass.createComment();
     return successResponse(res, 201, 'Comment posted.', result);
   },
+
+  /**
+   * @description Gets all comments for a particular question
+   * @param  {Object} req - The request object
+   * @param  {object} res - The response object
+   * @returns status code, message and all available comments
+   */
 
   getCommentsByQuestion: async (req, res) => {
     const { id } = req.params;
--- api/v1/controllers/questionscontroller.js
+++ api/v1/controllers/questionscontroller.js
@@ -3,20 +3,35 @@ import Meetup from '../models/Meetups';
 import { successResponse, errorResponse } from '../utils/responses';
 
 export default {
+  /**
+   * @description Creates a question for a particular meetup
+   * @param  {Object} req - The request object
+   * @param  {object} res - The response object
+   * @returns status code, message and question details
+   */
+
   createQuestion: async (req, res) => {
     const { id } = req.user;
     req.body.userid = id;
     const question = new Question(req.body);
 
-    question.title = question.title.replace(/([^a-zA-z0-9\s])/g, '');
-    question.body = question.body.replace(/([^a-zA-z0-9\s])/g, '');
+    question.title = question.title.replace(/([@#$%&<>*/\\\s])/g, '').trim();
+    question.body = question.body.replace(/([@#$%&<>*/\\\s])/g, '').trim();
 
     const meetupExists = await Meetup.getMeetupById(question.meetupid);
     if (!meetupExists) return errorResponse(res, 404, 'Meetup not found.');
 
     const result = await question.createQuestion();
+    result.topic = meetupExists.topic;
     return successResponse(res, 201, 'Your question has been recorded.', result);
   },
+
+  /**
+   * @description Upvotes a question
+   * @param  {Object} req - The request object
+   * @param  {object} res - The response object
+   * @returns status code, message and the upvoted question
+   */
 
   upvoteQuestion: async (req, res) => {
     const questionid = req.params.id;
@@ -41,6 +56,13 @@ export default {
     await Question.updateVotesTable(userid, questionid, vote);
     return successResponse(res, 200, `Question ${vote}.`, result);
   },
+
+  /**
+   * @description Downvotes a question
+   * @param  {Object} req - The request object
+   * @param  {object} res - The response object
+   * @returns status code, message and the upvoted question
+   */
 
   downvoteQuestion: async (req, res) => {
     const questionid = req.params.id;
