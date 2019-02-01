fa80b777105eda2eb52cc50252b0d5bf3e3ca0be
Christopher Iliyas <christopheriliyas@gmail.com>
2019-02-01 01:28:09 +0100

    feat(ui): consume admin meetups page [finishes #163634826]

--- UI/scripts/createmodal.js
+++ UI/scripts/createmodal.js
@@ -2,9 +2,6 @@ const simpleModal = document.getElementById('simple-modal');
 const modalBtn = document.getElementById('modal-button');
 const closeBtn = document.querySelector('.close_btn');
 
-modalBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
-
 function openModal() {
   simpleModal.style.display = 'block';
 }
@@ -12,3 +9,6 @@ function openModal() {
 function closeModal() {
   simpleModal.style.display = 'none';
 }
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
